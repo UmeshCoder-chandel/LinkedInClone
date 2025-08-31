b8a3cae58adb6193968e7104c10b84496296c786
Umesh Chandel <chandelumesh042@gmail.com>
2025-08-31 13:09:23 +0530

    Update

--- backend/authentication/auth.js
+++ backend/authentication/auth.js
@@ -9,7 +9,7 @@ const auth = async (req,res,next) => {
 console.log(req.cookies);
 
         
-        const token = req.cookies.access_token;
+        const token = req.cookies.access_token || req.headers.authorization && req.headers.authorization.split(" ")[1];
         if (!token) {
             return res.status(401).json({
                 message: "No token access"
--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -10,8 +10,8 @@ dotenv.config()
 
 const cookieOptions = {
     httpOnly: true,
-    secure: false,
-    sameSite: 'Lax',
+    secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     maxAge:7*24*60*60*1000
 }
 
