0ebe68835113bfc5bbd868c040ac53f86027cf6d
Umesh Chandel <chandelumesh042@gmail.com>
2025-08-31 12:41:35 +0530

    Update

--- backend/index.js
+++ backend/index.js
@@ -22,14 +22,14 @@ connectDB();
 const server = http.createServer(app)
 const io = new Server(server,{
     cors:{
-        origin:"http://localhost:5173",
+        origin:"https://linkedinclone-frontend.onrender.com",
         method:['GET','POST']
     }
 })
 
 app.use(cors({
     credentials:true,
-    origin:"http://localhost:5173"
+    origin:"https://linkedinclone-frontend.onrender.com"
 }))
 const PORT=process.env.PORT || 3000
  
