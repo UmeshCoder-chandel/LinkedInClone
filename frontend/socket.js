ae42ca3593f62fbb92593c372981c83a50f943d7
Umesh Chandel <chandelumesh042@gmail.com>
2025-08-31 13:16:02 +0530

    Update

--- frontend/socket.js
+++ frontend/socket.js
@@ -1,7 +1,7 @@
 import {io} from 'socket.io-client'
 
 
-const socket =io("https://linkedinclone-backend-i2bq.onrender.com")
+const socket =io("https://linkedinclone-backend-i2bq.onrender.com",{withCredentials:true})
 
 
 export default socket;
